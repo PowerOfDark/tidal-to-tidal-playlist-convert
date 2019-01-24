1915365b94ce7fd4c3f8a9c7e44b32339fe41f53
SimDoes <simasmacys.doesminecraft@gmail.com>
2019-01-24 18:00:04 +0000

    fix typo

--- tidalcopy.py
+++ tidalcopy.py
@@ -8,7 +8,7 @@ p2 = input("Enter password of account you are copying to.")
 session2 = tidalapi.Session()
 session2.login(u2,p2)
 id1 = input("Enter the user.id of the 1st account.") # Found by using Fiddler or somewhere in inspect element.
-id1 = input("Enter the user.id of the 2nd account.") # Found by using Fiddler or somewhere in inspect element.
+id2 = input("Enter the user.id of the 2nd account.") # Found by using Fiddler or somewhere in inspect element.
 fav = tidalapi.Favorites(session, id1)
 fav2 = tidalapi.Favorites(session2, id2)
 tracks = fav.tracks()
