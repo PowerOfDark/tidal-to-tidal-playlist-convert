62d5d940c20d0b346cecf36cae4c45536e251f8f
SimDoes <simdoesmail@pm.me>
2020-04-29 18:27:35 +0100

    Handle tracks that no longer exist
    handle exceptions
    + print track name instead of id

--- favcopy.py
+++ favcopy.py
@@ -1,10 +1,10 @@
 import tidalapi
 session = tidalapi.Session()
-u1 = raw_input("Enter username of account you are copying from: ")
-p1 = raw_input("Enter password of account you are copying from: ")
+u1 = input("Enter username of account you are copying from: ")
+p1 = input("Enter password of account you are copying from: ")
 session.login(u1,p1)
-u2 = raw_input("Enter username of account you are copying to: ")
-p2 = raw_input("Enter password of account you are copying to: ")
+u2 = input("Enter username of account you are copying to: ")
+p2 = input("Enter password of account you are copying to: ")
 session2 = tidalapi.Session()
 session2.login(u2,p2)
 id1 = session.user.id
@@ -13,5 +13,7 @@ fav = tidalapi.Favorites(session, id1)
 fav2 = tidalapi.Favorites(session2, id2)
 tracks = fav.tracks()
 for track in tracks:
-    print(track.id)
-    fav2.add_track(track.id)
+    try:
+        print(track.name)
+        fav2.add_track(track.id)
+    except Exception as e: print(e)
