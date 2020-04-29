003f0751d678eb60dc7418b7afc45a56aee2bf19
SimDoes <simdoesmail@pm.me>
2020-04-29 18:44:00 +0100

    add acounter

--- favcopy.py
+++ favcopy.py
@@ -12,8 +12,24 @@ id2 = session2.user.id
 fav = tidalapi.Favorites(session, id1)
 fav2 = tidalapi.Favorites(session2, id2)
 tracks = fav.tracks()
+a = []
+i = 0
+b = 0
 for track in tracks:
     try:
         print(track.name)
         fav2.add_track(track.id)
-    except Exception as e: print(e)
+        i += 1
+    except Exception as e:
+        print(e)
+        a.append((track.id) + " // " + (track.name))
+        b += 1
+print(i)
+print(a)
+print(b)
+fav3 = tidalapi.Favorites(session2, id2)
+tracks3 = fav3.tracks()
+count = 0
+for track in tracks:
+    count += 1
+print(str(count) + " on 2nd acc")
